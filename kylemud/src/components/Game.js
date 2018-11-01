149cc33480ecb499281a6602102c1a64d60801b2
meltzerkyle <meltzerkyle@gmail.com>
2018-10-31 21:38:30 -0500

    added pusher

--- kylemud/src/components/Game.js
+++ kylemud/src/components/Game.js
@@ -1,5 +1,18 @@
 import React, { Component } from "react";
+import {
+  Row,
+  Col,
+  Card,
+  CardBody,
+  CardTitle,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Button
+} from "reactstrap";
 import axios from "axios";
+import Pusher from "pusher-js";
 
 class Game extends Component {
   constructor(props) {
@@ -21,17 +34,40 @@ class Game extends Component {
       .get("https://kylemud.herokuapp.com/api/adv/init/", header)
       .then(response => {
         console.log(response);
+        this.startPusher(response.data.uuid);
         this.setState({
-          players: response.data.players
+          players: response.data.players,
+          name: response.data.name,
+          roomTitle: response.data.title,
+          roomDescription: response.data.description
         });
       })
       .catch(error => alert(error.response.data.error));
   }
+  startPusher(id) {
+    let pusher = new Pusher("5fe57c4fbb2708238bac", {
+      cluster: "us2",
+      forceTLS: true
+    });
+
+    let channel = pusher.subscribe(`p-channel-${id}`);
+
+    channel.bind("my-event", function(data) {
+      alert(JSON.stringify(data));
+    });
+
+    console.log('test string')
+  }
   render() {
     return (
       <div>
-        hello game
+        <div>GAMEWORLD</div>
         <div>
+          You, {this.state.name}, are in the {this.state.roomTitle}.{" "}
+          {this.state.roomDescription}
+        </div>
+        <div>
+          Other players in the room:
           {this.state.players.map(player => (
             <div>{player}</div>
           ))}
